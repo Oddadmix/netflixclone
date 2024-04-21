dc9c949c4e1f16cdaa565e5f4c6056ec88e07868
Ahmed Wasfy <ahmedwasfy@Ahmeds-MacBook-Pro.local>
2024-04-21 18:48:57 -0400

    Login and Signup

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { DB } from '@/lib/db';
 import User from '@/lib/models/user';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -26,14 +27,31 @@ export const authOptions = {
         console.log(credentials);
         await DB.connect();
         const user = await User.findOne({ email: credentials!.username });
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        if (!user) {
+          // Create the user
+          console.log('User not found, creating user');
+          const hashedPassword = await bcrypt.hash(credentials!.password, 10);
+
+          const newUser = new User({
+            email: credentials!.username,
+            password: hashedPassword,
+            provider: 'credentials',
+          });
+
+          await newUser.save();
+
+          return newUser;
+        } else {
+          const match = await bcrypt.compare(
+            credentials!.password,
+            user.password
+          );
+          if (match) {
+            return user;
+          } else {
+            return null;
+          }
         }
       },
     }),
