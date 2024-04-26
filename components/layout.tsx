e27f15154951b75b72c3fb7dda2f063da5b3068d
Ahmed Wasfy <ahmedwasfy@Ahmeds-MacBook-Pro.local>
2024-04-26 14:25:37 -0400

    only show search for loggedin users

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Logo from './logo';
 import '../app/globals.css';
 import Search from './search';
+import { useSession } from 'next-auth/react';
 
 export default function Layout({
   children,
@@ -11,12 +12,14 @@ export default function Layout({
   extraClasses?: string;
   showSearch?: boolean;
 }) {
+  const { data } = useSession();
+
   return (
     <div className={`flex min-h-screen flex-col ${extraClasses}`}>
       <div className='min-h-screen bg-black bg-opacity-50'>
         <div className='flex justify-between p-4'>
           <Logo />
-          <div className='mt-7'>{showSearch && <Search />}</div>
+          {data && <div className='mt-7'>{showSearch && <Search />}</div>}
         </div>
         {children}
       </div>
